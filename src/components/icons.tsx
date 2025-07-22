f4f2c16efb1556446c051c180dd4132a0d7fca25
Krzysztof <krzysztofjofficial@gmail.com>
2025-07-22 11:32:09 +0000

    Dobra wez to H jednak zastap takim rodzajem H jak jest obrazek w rezerwa

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,14 +3,17 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
         width="40"
         height="40"
-        viewBox="0 0 40 40"
+        viewBox="0 0 24 24"
         fill="none"
+        stroke="hsl(var(--primary))"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-        <path
-            d="M5 0V40H15V25H25V40H35V0H25V15H15V0H5Z"
-            fill="hsl(var(--primary))"
-        />
+        <path d="M6 3v18" />
+        <path d="M18 3v18" />
+        <path d="M6 12h12" />
     </svg>
 );
