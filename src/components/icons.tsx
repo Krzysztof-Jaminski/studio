53a89377743730f35da91f75f67c6c60e065b0e3
Krzysztof <krzysztofjofficial@gmail.com>
2025-07-22 11:34:25 +0000

    Nie wazne dodaj drogi H do tego H co juz jest przesun je troche w prawo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
         width="40"
         height="40"
-        viewBox="0 0 24 24"
+        viewBox="0 0 28 24"
         fill="none"
         stroke="hsl(var(--primary))"
         strokeWidth="2.5"
@@ -12,8 +12,15 @@ export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
-        <path d="M6 3v18" />
-        <path d="M18 3v18" />
-        <path d="M6 12h12" />
+        <g opacity="0.7">
+            <path d="M6 3v18" />
+            <path d="M18 3v18" />
+            <path d="M6 12h12" />
+        </g>
+        <g>
+            <path d="M10 3v18" />
+            <path d="M22 3v18" />
+            <path d="M10 12h12" />
+        </g>
     </svg>
 );
