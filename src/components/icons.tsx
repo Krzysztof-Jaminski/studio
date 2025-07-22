58c0f8d7b9b687b6579421e9058f6760aff792ea
Krzysztof <krzysztofjofficial@gmail.com>
2025-07-22 10:02:31 +0000

    Na poczatku jest takie paskudne PH na stronei wjesciowej logo jest rozci

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,8 @@
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-    <div {...props} className="flex items-center justify-center space-x-1 font-bold text-2xl font-headline">
+    <div {...props}>
         <div className="flex items-center justify-center h-full w-full rounded-md bg-gradient-to-br from-blue-500 to-purple-600 text-white p-1">
-            <span className="font-black">PH</span>
+            <span className="font-black font-headline text-2xl">PH</span>
         </div>
     </div>
 );
