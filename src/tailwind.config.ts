eb0bcf9c8e51ecf483ac21c71f5953bb2bce0a03
Krzysztof <krzysztofjofficial@gmail.com>
2025-07-22 10:13:46 +0000

    Wez zmien czcionkę calej apliacji na RussoOne

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"Russo One"', 'sans-serif'],
+        headline: ['"Russo One"', 'sans-serif'],
       },
       colors: {
         border: "hsl(var(--border))",
